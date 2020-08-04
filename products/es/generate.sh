695e19023696bdae6e0607654da907cd850d6c9b
Luis Guillen Civera <luisguillenc@gmail.com>
2020-08-04 10:42:33 +0200

    first commit

---
+++ products/es/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SRCDIR=$(dirname $(readlink -f "$0"))
+OUTDIR=${SRCDIR}/output
+
+export PRODUCT_LANG="es"
+export PRODUCT_VERSION=$(git describe --match 'v[0-9]*' --dirty='.m' --always)
+export PRODUCT_BUILD=$(date +%F)
+
+# list of products
+PRODUCTS=""
+
+mkdir -p $OUTDIR
+for product in $PRODUCTS; do
+	echo "Constructing: ${product}"
+	pushd ${SRCDIR}/${product} >/dev/null
+	./generate.sh
+	mv output.pdf ${OUTDIR}/${product}_${PRODUCT_LANG}.pdf
+	popd >/dev/null
+done
+
